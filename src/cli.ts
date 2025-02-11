a5c7033550bcff07d2b972215154f1b3631ad898
Cameron Wheeler <1223458+cwheel@users.noreply.github.com>
2025-02-11 17:52:16 -0500

    Add cli option for plt file analysis

--- src/cli.ts
+++ src/cli.ts
@@ -10,10 +10,15 @@ import geojsonFromPlot from './geojson/geojson.js';
 const argv = yargs(hideBin(process.argv))
     .command('$0 <input>', 'Convert PLT to GeoJSON')
     .positional('input', {
-            describe: 'Input Compass PLT file',
-            type: 'string',
-            demandOption: true,
-        })
+        describe: 'Input Compass PLT file',
+        type: 'string',
+        demandOption: true,
+    })
+    .option('analyze', {
+        alias: 'a',
+        describe: 'Analyze the Compass file instead of converting it to GeoJSON',
+        type: 'boolean',
+    })
     .demandCommand(1)
     .parseSync();
 
@@ -25,4 +30,19 @@ if (!argv.input.endsWith('.plt')) {
 const plt = await fs.readFile(argv.input, 'utf8');
 const plot = parse(plt);
 
-console.log(JSON.stringify(geojsonFromPlot(plot), null, 2));
+if (argv.analyze) {
+    console.log('----------------------------------------');
+    console.log('Name:', plot.name);
+    console.log('Datum:', plot.datum.name);
+    console.log('Root Stations:', plot.stations.length);
+    console.log('Child Stations:', plot.stations.reduce((acc, station) => acc + station.stations.length, 0));
+    console.log('Hidden Stations:', plot.stations.reduce((acc, station) => acc + station.stations.filter(station => station.flags.ExcludePlotting || station.flags.TotalExclusion).length, 0));
+    console.log('----------------------------------------');
+    console.log('Surveys:', plot.surveys.length);
+    console.log('');
+    plot.surveys.forEach((survey) => {
+        console.log(`Survey ${survey.name} on`, survey.date.toDateString());
+    });
+} else {
+    console.log(JSON.stringify(geojsonFromPlot(plot), null, 2));
+}
